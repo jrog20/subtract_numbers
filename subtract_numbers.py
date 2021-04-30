549f915bf2294847357c0cbfeacf3d5c0de26a5b
Jaime Rogstad <66330786+jrog20@users.noreply.github.com>
2021-04-30 08:46:12 -0400

    Update subtract_numbers.py

--- subtract_numbers.py
+++ subtract_numbers.py
@@ -9,20 +9,14 @@ def main():
     """
     Calculate the first number minus the second number.
     """
-
-    # Tell the user what the program does
+    
     print("This program subtracts one number from another.")
 
-    # Prompt user to enter two numbers
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
-
-    # Calculate first number minus second number
     answer = num1 - num2
 
-    # Give result to user
-    print("The result is " + str(answer))
-
+    print("The result is", answer)
 
 if __name__ == '__main__':
     main()
